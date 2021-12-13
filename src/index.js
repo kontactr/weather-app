24c71648e606991e0b595c918b2dacf08d40baad
kontactr <gandhiyash1381997@gmail.com>
2021-12-13 21:58:51 +0530

    test: netlify env variables test completed

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import { WeatherPage } from 'pages';
 import './index.css';
 
-console.log("NETLIFY TEST: ",process.env.REACT_APP_TEST);
-
 ReactDOM.render(
   <React.StrictMode>
     <WeatherPage />
